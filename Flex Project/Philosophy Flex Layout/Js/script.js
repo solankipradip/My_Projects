569f5dad1aa5303216d109ff5fd4b1df14d1ef3d
solankipradip <solankipradip096@gmail.com>
2023-05-21 10:19:08 +0530

    Added some projects files to repository

---
+++ Flex Project/Philosophy Flex Layout/Js/script.js
@@ -0,0 +1,11 @@
+const navToggle= document.getElementById('menu-icon');
+const navLinks= document.getElementById('nav-links');
+
+navToggle.addEventListener('click',() =>{
+    navLinks.classList.toggle('active');
+})
+
+
+
+      
+
