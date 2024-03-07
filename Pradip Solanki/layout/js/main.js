27d845ac2b81657cc8e4a8771102bbb0c0e12645
solankipradip <solankipradip096@gmail.com>
2024-03-07 07:23:18 +0530

    update

--- Pradip Solanki/layout/js/main.js
+++ Pradip Solanki/layout/js/main.js
@@ -1,72 +1,55 @@
-
+// Owlcarousel
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-            items:3,
-            loop:true,
-            margin:10,
-            dots:false,
-            responsive:{
-                0:{
-                    items:1
-                },
-                600:{
-                    items:2
-                },
-                900:{
-                    items:3
-                }
-            }
-        })
-
-    $("#x").owlCarousel({
-            loop:true,
-          // margin:10,
-          nav:true,
-          autoplay:true,
-          autoplayTimeout:3000,
-          // autoplayHoverPause:true,
-          center: true,
-          navText: [
-              "<i class='fa fa-angle-left'></i>",
-              "<i class='fa fa-angle-right'></i>"
-          ],
-          responsive:{
-              0:{
-                  items:1
-              },
-              600:{
-                  items:1
-              },
-              1000:{
-                  items:3
-              }
+    $("#carousel-1").owlCarousel({
+        loop:true,
+      margin:10,
+      nav:true,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:false,
+      center: true,
+      navText: [
+          "<i class='fa fa-angle-left'></i>",
+          "<i class='fa fa-angle-right'></i>"
+      ],
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:1
+          },
+          1000:{
+              items:3
           }
-        });
-    
-    
-
-
+      }
     });
 
-   
-    var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:3,
-    loop:true,
-    margin:10,
-    dots:false,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        900:{
-            items:3
+    //////////////////
+
+    $('.owl-carousel-2').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3,
+            },
+            1000:{
+                items:3,
+                loop:false
+            }
         }
-    }
-});
+    })
+
+
+  });
+
+  var owl = $('.owl-carousel-2');
+owl.owlCarousel();
 // Go to the next item
 $('.customNextBtn').click(function() {
     owl.trigger('next.owl.carousel');
@@ -77,11 +60,3 @@ $('.customPrevBtn').click(function() {
     // Parameters has to be in square bracket '[]'
     owl.trigger('prev.owl.carousel', [300]);
 })
-
-
-
-
-
-
-
-
